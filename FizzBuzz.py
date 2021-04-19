c738ded7a9b745506900d41cb5a73a7e61af500c
MaTko-987 <kwikzzstep@gmail.com>
2021-04-19 21:10:16 +0200

    Initial commit

---
+++ FizzBuzz.py
@@ -0,0 +1,17 @@
+
+
+
+x = input("Select a number between 1 and 100: ")
+
+x = int(x)
+
+
+for num in range(1, x+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
---
+++ MakeStringsLowerCase.py
@@ -0,0 +1,7 @@
+text = 'Today Is A BeautiFul DAY'
+
+print("Text: ")
+print("Today Is A BeautiFul DAY")
+
+print("Converted text: ")
+print(text.lower())
